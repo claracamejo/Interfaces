3dec064494cb94b616a21b45af0715bb8fc50b81
ClaraCamejo <clara.camejo@hotmail.com>
2020-09-07 20:49:01 -0300

    ejercicio 5

--- TP1/ejercicio5/index.js
+++ TP1/ejercicio5/index.js
@@ -1,16 +1,30 @@
 let ctx = document.querySelector("#mycanvas").getContext("2d");
 
-let widht = 255;
-let height = 255;
-let imageData = ctx.createImageData(widht, height);
+let width = 500;
+let height = 500;
+let imageData = ctx.createImageData(width, height);
+let color;
 let r = 0;
 let g = 0;
 let b = 0;
 let a = 255;
 
-for (let x = 0; x < widht; x++) {
+for (let x = 0; x < width; x++) {
+  let coef = (x - width / 2) / (width / 2);
+
+  if (x <= width / 2) {
+    color = 255 * (1 + coef);
+    r = color;
+    g = color;
+    b = 0;
+  } else {
+    color = 255 * (1 - coef);
+    r = 255;
+    g = color;
+    b = 0;
+  }
+
   for (let y = 0; y < height; y++) {
-    (r = y), (g = y), (b = y);
     setPixel(imageData, x, y, r, g, b, a);
   }
 }
