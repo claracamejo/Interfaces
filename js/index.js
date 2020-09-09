f698dbd1164d7c9319c5859e0e97ebe2ab63faa3
ClaraCamejo <clara.camejo@hotmail.com>
2020-09-09 19:46:20 -0300

    lapiz y color

--- js/index.js
+++ js/index.js
@@ -1,13 +1,41 @@
-let canvas = document.querySelector("#mycanvas");
-let ctx = canvas.getContext("2d");
+document.addEventListener("DOMContentLoaded", function (event) {
+  console.log("DOM fully loaded and parsed");
 
-canvas.addEventListener("mousedown", dibujar(event));
+  let canvas = document.querySelector("#mycanvas");
+  let ctx = canvas.getContext("2d");
+  let colorPicker = document.querySelector("#color");
 
-function dibujar(e) {
-  let x = e.clientX;
-  let y = e.clientY;
-  ctx.strokeStyle = "blue";
-  ctx.lineWidth = 10;
-  ctx.lineTo(x, y);
-  ctx.stroke();
-}
+  colorPicker.addEventListener("change", watchColorPicker, false);
+
+  function watchColorPicker(event) {
+    ctx.strokeStyle = event.target.value;
+    console.log("en ingles");
+  }
+
+  let pintando = false;
+
+  function posInicio(e) {
+    pintando = true;
+    dibujando(e);
+  }
+
+  function posFin() {
+    pintando = false;
+    ctx.beginPath();
+  }
+
+  function dibujando(e) {
+    if (!pintando) return;
+    ctx.lineWidth = 10; //px del lapiz;
+    ctx.lineCap = "round"; //forma del lapiz;
+    // ctx.strokeStyle = color; //color del lapiz
+    ctx.lineTo(e.clientX, e.clientY);
+    ctx.stroke();
+    ctx.beginPath();
+    ctx.moveTo(e.clientX, e.clientY);
+  }
+
+  canvas.addEventListener("mousedown", posInicio);
+  canvas.addEventListener("mouseup", posFin);
+  canvas.addEventListener("mousemove", dibujando);
+});
